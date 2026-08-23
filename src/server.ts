fcb77d3427db8e2c2c4f6b42ead65fd73f903e87
ribeiroLevi <leviribeiro72@gmail.com>
2026-08-23 16:29:06 -0300

    feat: backend setup

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import {
+  validatorCompiler,
+  serializerCompiler,
+  ZodTypeProvider,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { routes } from "./routes.js";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, { origin: "*" });
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Ecommerce Web 123",
+      version: "0.0.1",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+//rota de documentação da API
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.get("/", () => {
+  return "Hello World!";
+});
+
+app.register(routes);
+
+//porta da API
+app.listen({ port: 3333 }).then(() => {
+  console.log("Servidor rodando!");
+});
